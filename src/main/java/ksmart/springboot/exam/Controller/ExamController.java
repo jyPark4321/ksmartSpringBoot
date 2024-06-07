91d0789b015fcda920f8de9101259097190e65a5
jyPark4321 <qkrwnddus93@gmail.com>
2024-06-07 17:26:19 +0900

    "24-06-07" 스프링부트 오후수업 마무리

--- src/main/java/ksmart/springboot/exam/Controller/ExamController.java
+++ src/main/java/ksmart/springboot/exam/Controller/ExamController.java
@@ -1,48 +1,94 @@
-package ksmart.springboot.exam.Controller;
+package ksmart.springboot.exam.controller;
 
-import ksmart.springboot.exam.dto.Member;
-import ksmart.springboot.exam.service.ExamService;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import jakarta.annotation.PostConstruct;
+import ksmart.springboot.exam.dto.Member;
+import ksmart.springboot.exam.service.ExamService;
 /**
- * @Controller : 주소의 요청과 응답을 담당하는 클래스(HTTP 통신)
- *               VIEW의 경로가 정해져 있다.(String : 뷰의 논리적인 경로)
+ * @Controller : 주소의 요청과 응답을 담당하는 클래스 (HTTP 통신)
+ * 				VIEW의 경로가 정해져 있따. (String : 뷰의 논리적인 경로)
+ *
+ *
+ * 클래스위에 @Component 작성하게 되면 spring freamwork가 관리하는 객체로 생성 (sprging bean)
+ * @Component 계열 : @Controller, @Service, @Repositoty(DAO), @Mapper(DAO), @Configuration(스프링설정)
+ * 				  : 일반적인 클래스를 spring bean 등록하고싶으면 @Component
+ *
  */
-
 @Controller
 public class ExamController {
-    @RequestMapping(value = "/exam/exam2", method = RequestMethod.GET)
-    public ModelAndView exam2() {
-        ExamService examService =  new ExamService();
 
+    // DI (Dependency Injection : 의존성 주입)
+    // 의존성주입 : 클래스 간의 관계를 형성, 하나의 객체가 다른객체의 의존성을 제공하는 테크닉
+    // 의존성은 서비스로 사용할 수 있는 객체
+
+    // 1. 필드 주입방식 (안씀)
+    //@Autowired
+    //private ExamService examService;
+
+    // 2. 수정자 메소드 주입방식
+    //private ExamService examService;
+
+    //@Autowired
+    //public void setExamService(ExamService examService) {
+    //	this.examService = examService;
+    //}
+
+    // 3. 생성자 메소드 주입방식 (이거 사용)
+    private final ExamService examService;
+
+    public ExamController(ExamService examService) {
+        this.examService = examService;
+    }
+
+
+    //@PostConstruct : spring 객체를 생성한 후에 이벤트를 실행하는 어노테이션
+    @PostConstruct
+    public void initExamController() {
+        System.out.println("ExamController 객체생성");
+    }
+
+
+    //이게 @GetMapping 구식버전
+    @RequestMapping(value="/exam/exam2", method = RequestMethod.GET)
+    public ModelAndView exam2() {
+        //이따구로 쓰면 쓸때마다 객체생성하고 사용해야함
+        //ExamService examService = new ExamService();
+
+        List<Member> memberList = examService.getMemberList();
+        System.out.println(memberList);
+
+
+        //프레임워크가 객체생성함 @Service
 
         ModelAndView mav = new ModelAndView();
-        mav.addObject("title", "예제2");
-        mav.setViewName("exam/exam2");
-
+        mav.addObject("title", "예제2"); //화면에 전달할 data를 셋팅하는 방법
+        mav.setViewName("/exam/exam2");
         return mav;
     }
 
+
+
     /**
-     * @GetMapping : GET 방식의 HTTP 주소요청시 특정핸들러 메소드와 매핑시켜주는 어노테이션
-     * @return => 리턴은 VIEW 경로와 연관되어있다. String : 화면의 논리적 경로
+     * @GetMappin : GET방식의 HTTP 주소요청시 틍적핸들러 메소드와 맵핑시켜주는 어노테이션
+     * @return : VIEW 경로와 연관되어있다  String : 화면의 물리적 경로
      */
     @GetMapping("/exam/exam1")
-    public String exam1(Model model) {
-
+    public String eaxm1(Model model) {
         Member memberInfo = new Member("id001","pw001","홍01","관리자","010-1111-1111");
 
-        // Model의 역활 : 화면에 전달될 데이터를 가지고 있는 객체
-        // model.addAllAttributes("키","값");
-        model.addAttribute("title", "예제1");
-        model.addAttribute("memberInfo", memberInfo);
+        // Model의 역할 : 화면에 전달될 데이터를 가지고 있는 객체
+        // model.addAttribute("키","값");
+        model.addAttribute("title","예제1");
+        model.addAttribute("memberInfo",memberInfo);
 
         return "exam/exam1";
     }
--- src/main/java/ksmart/springboot/exam/service/ExamService.java
+++ src/main/java/ksmart/springboot/exam/service/ExamService.java
@@ -1,53 +1,52 @@
 package ksmart.springboot.exam.service;
 
-import ksmart.springboot.exam.dto.Member;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import ksmart.springboot.exam.dto.Member;
+
 /**
  * @Service : 비즈니스 로직 수행, 기능구현, data 가공
- *            (트랜잭션 : 논리적인 작업을 수행하는 한 단위)
+ * 			(트랜잭션 : 논리적인 작업을 수행하는 한단위)
+ * 			bean의 이름 : ExamService - > examService
  */
 @Service
 public class ExamService {
 
     public ExamService() {
-        System.out.println("객체생성 ");
+        System.out.println("객체생성");
     }
 
-    public List<Member> getmemberList(){
-        // Member id001~id009까지 arrayList 만들어보세요.
-        // id001 관리자 id002 판매자 id003 구매자
-        List<Member> memberList = new ArrayList<Member>();
+    public List<Member> getMemberList(){
+
+        //Member id001~ id009 arrayList 만들어보세요
+        List<Member> memberList =new ArrayList<>();
+        String[] levelList = new String[]{"관리자","판매자","구매자"};
         Member member = null;
-        for(int i = 0; i<10; i++){
-            String memberId = "id00"+i;
-            String memberPw = "pw00"+i;
-            String memberName = "홍0"+i;
-            String memberLevel = "";
-            int num = i % 3;
-            if(num == 0){
-                memberLevel = "관리자";
-            } else if(num == 1){
-                memberLevel = "판매자";
-            } else {
-                memberLevel = "구매자";
-            }
-            String memberMobile = "010-"+i+i+i+i+"-"+i+i+i+i;
-            member = new Member(memberId,memberPw,memberName,memberLevel,memberMobile);
+        for(int i=1; i<10;i++) {
+            String memberId = "id00" +i;
+            String memberPw = "pw00" +i;
+            String memberLevel = levelList[(i-1)% levelList.length];
+            String memberName = "홍00" +i;
+            String memberMobile = "010- "+i+i+i+i+"-"+i+i+i+i;
+            member = new Member(memberId,memberPw,memberLevel,memberName,memberMobile);
             memberList.add(member);
         }
         return memberList;
     }
-    /*
-    public void payOrder(String memberId, Goods goods, Order order, Payment pay){
-        //1. 주문 내역확인 상품 재고 확인
-        //2. 주문 내역 결재 금액 충분
-        //3. 상품 재고 업데이트
-        //4. 주문 테이블에 주문 이력 추가
-    }
-    */
 
+	/*
+	public void payOrder(String memberId, Goods goods, Order order,Paymenet pay) {
+		//1. 주문 내역확인 상품 재고 확인
+
+		//2. 주문 내역 결제금액 충분?
+
+		//3. 상품 재고 업데이트
+
+		//4. 주문 주문이력추가
+
+	}
+	*/
 }
